4fbb4ddbed8c977f70d655c6457495a470c30217
Indytskyi <ind.artem2003@gmail.com>
2022-09-18 15:08:17 +0300

    GGO-LVIV-T-139 [Indytskyi] Create new validation for saving in S3.

--- src/main/java/com/project/indytskyi/tripsservice/validations/ImageValidation.java
+++ src/main/java/com/project/indytskyi/tripsservice/validations/ImageValidation.java
@@ -12,9 +12,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageValidation {
 
     /**
-     * Validation of
+     * Validation for pictures that saving in Amazon S3
+     * Images must be:
+     * only jpg or png format
+     * Size of one picture must be less than 3 mb
+     * The number of photos must be exactly 3"
      */
     public List<ErrorResponse> validateImages(List<MultipartFile> files) {
+        final int numberOfPictures = 3;
+        final int sizeOfOnePicture = 3145728;
         List<ErrorResponse> errorResponses = new ArrayList<>();
         files.forEach(file -> {
             String imageName = file.getOriginalFilename();
@@ -24,11 +30,16 @@ public class ImageValidation {
                         "Incorrect format of file. Only images with format (jpg or png)"));
             }
 
-            if (file.getSize() > 3145728) {
+            if (file.getSize() > sizeOfOnePicture) {
                 errorResponses.add(new ErrorResponse(imageName,
                         "Incorrect size of file. Must be less than 3 mb"));
             }
         });
+
+        if (files.size() != numberOfPictures) {
+            errorResponses.add(new ErrorResponse("number",
+                    "The number of photos must be exactly 3"));
+        }
 
         return errorResponses;
     }
